ec44296e87ccb1a4f728577a73ca98533b5db3bf
Yisely <ybarraz3@asu.edu>
2022-02-13 19:30:01 -0700

    Changed code from C++ to Python
    Decided to code in Python for the simplicity

---
+++ UDPClient.py
@@ -0,0 +1,34 @@
+import socket
+
+ClientSocket = socket.socket()
+host = '10.120.70.106'
+port = 1233
+
+print('Waiting for connection')
+try:
+    ClientSocket.connect((host, port))
+except socket.error as e:
+    print(str(e))
+
+ans = 'open'
+
+Response = ClientSocket.recv(1024)
+while ans != 'exit':
+    ans = input('\nEnter a command:')
+    
+    if ans == 'query games' or ans == 'query players':
+        ClientSocket.send(str.encode(ans))
+        Response = ClientSocket.recv(1024)
+        print(Response.decode('utf-8'))
+    elif ans[0:12] == 'de-register ':
+        ClientSocket.send(str.encode(ans))
+        Response = ClientSocket.recv(1024)
+        print(Response.decode('utf-8'))
+    elif ans[0:9] =='register ':
+        ClientSocket.send(str.encode(ans))
+        Response = ClientSocket.recv(1024)
+        print(Response.decode('utf-8'))
+    elif ans != 'exit':
+        print('not a valid command try again')
+
+ClientSocket.close()
---
+++ UDPServer.py
@@ -0,0 +1,63 @@
+import socket
+import os
+from _thread import *
+
+ServerSocket = socket.socket()
+host = '10.120.70.106'
+port = 1233
+players = []
+games = []
+
+try:
+    ServerSocket.bind((host, port))
+except socket.error as e:
+    print(str(e))
+
+print('Waitiing for a Connection..')
+ServerSocket.listen(5)
+
+
+def threaded_client(connection):
+    connection.send(str.encode('Welcome to the Server!'))
+    while True:
+        data = connection.recv(2048)
+        decodeddata = data.decode('utf-8')
+        reply = 'Server Says: ' + data.decode('utf-8')
+        if decodeddata == 'query games':
+            if games:
+                reply = str(len(games)) + '\n' #the ammount of players
+                reply += str(games)
+            else:
+                reply = '0\n[]'
+        elif decodeddata == 'query players':
+            global players
+            if players:
+                reply = str(len(players)) + '\n' #the ammount of players
+                reply += str(players)
+            else:
+                reply = '0\n[]'
+        elif decodeddata[0:9] == 'register ':
+            reply = 'FAILURE'
+            
+            player = decodeddata.split(' ')
+            player.remove('register')
+            players.append(player)
+            reply = 'SUCCESS'
+        elif decodeddata[0:12] == 'de-register ':
+            reply = 'FAILURE'
+            initialLength = len(players)
+            players = [i for i in players if i[0] != decodeddata[12:]]
+            resultLength = len(players)
+            if initialLength != resultLength:
+                reply = 'SUCCESS'
+        else:
+            reply = 'error'
+        connection.sendall(str.encode(reply))
+    connection.close()
+
+while True:
+    Client, address = ServerSocket.accept()
+    print('Connected to: ' + address[0] + ':' + str(address[1]))
+    start_new_thread(threaded_client, (Client, ))
+
+ServerSocket.close()
